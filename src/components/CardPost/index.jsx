503c748398e58d5b745395d3c578075ba6b2f8e0
Ayron Paulo de Souza Sant' Anna <ayronpaulo2r@gmail.com>
2026-01-19 11:58:14 -0300

    feat(components): adiciona componente CardPost

---
+++ src/components/CardPost/index.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image"
+
+export const CardPost = ({ post }) => {
+  return(
+    <article>
+      <header>
+        <figure>
+          <Image />
+        </figure>
+      </header>
+
+      <section>
+        titulo
+        texto
+      </section>
+
+      <footer></footer>
+    </article>
+  )
+}
