53f2a4936e7c37a7ba430d0b7c4d5db67cfbc6a3
Ayron Paulo de Souza Sant' Anna <ayronpaulo2r@gmail.com>
2026-01-19 11:09:15 -0300

    feat(components): adiciona componente Aside e aside.module.css

---
+++ src/components/Aside/index.jsx
@@ -0,0 +1,8 @@
+import styles from './aside.modules.css'
+export const Aside = () => {
+    return (
+      <aside>
+          <img src="/logo.svg" alt="Logo da Code Connect" />
+      </aside>
+    )
+}
